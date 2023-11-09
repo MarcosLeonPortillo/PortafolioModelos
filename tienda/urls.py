5f5a6929847929fc088fa4490da512bc3c978d82
ciclom <MarcosLeonPortillo>
2023-11-09 14:52:10 +0100

    ProductosNoListados

--- tienda/urls.py
+++ tienda/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('tienda/', views.welcome, name='welcome'),
+    path('tienda/admin', views.admin, name='admin'),
+    path('tienda/admin', views.editar, name="editar")
 ]
+
+
--- tienda/views.py
+++ tienda/views.py
@@ -1,5 +1,40 @@
 from django.shortcuts import render
+from .models import Producto
+from .models import Compra
+from .models import Marca
 
 # Create your views here.
+
 def welcome(request):
     return render(request,'tienda/index.html', {})
+
+
+
+
+
+def admin(request):
+    Productos = Producto.objects.all()
+    return render(request, 'tienda/Productos.html', {})
+
+def editar(request):
+    Productos = Producto.objects.all()
+    return render(request, 'tienda/editar.html', {})
+
+def eliminar(request):
+    Productos = Producto.objects.all()
+    return render(request, 'tienda/editar.html', {})
+
+def nuevo(request):
+    Productos = Producto.objects.all()
+    return render(request, 'tienda/nuevo.html', {})
+
+
+
+
+
+def compras(request):
+    Compras = Compra.objects.all()
+    return render(request, 'tienda/nuevo.html', {})
+
+
+
