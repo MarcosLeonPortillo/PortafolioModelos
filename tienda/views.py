a941855b680d28e4571392c4c9c10bd00b00586b
ciclom <MarcosLeonPortillo>
2023-11-16 14:55:17 +0100

    Producto sya listados

--- tienda/views.py
+++ tienda/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Producto
 from .models import Compra
+from django.shortcuts import get_object_or_404
 from .models import Marca
 
 # Create your views here.
@@ -15,9 +16,18 @@ def admin(request):
     return render(request, 'tienda/Productos.html', {'Productos': Productos})
 
 def editar(request, pk):
-    Productos = Producto.objects.all()
+    Productos = get_object_or_404(Producto, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=Producto)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('editar', pk=post.pk)
+    else:
+        form = PostForm(instance=Producto)
     return render(request, 'tienda/editar.html', {'Productos': Productos})
-
 def eliminar(request):
     Productos = Producto.objects.all()                                              
     return render(request, 'tienda/editar.html', {})
